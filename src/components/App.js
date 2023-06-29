d7be3af283dfc37b92dc8d0abeee8ca20ccc7450
Sanjay Kumar <sanjaykr0410@gmail.com>
2023-06-30 00:54:19 +0530

    Re attempt

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,8 @@ const App = () => {
   const heading = {};
   return (
     <div id="main">
-      <h1 className="styles.success">Success</h1>
-      <h1 id="error" style={{ fontSize: "50px", color: "red" }}>
+      <h1 className={styles.success}>Success</h1>
+      <h1 id="error" style={{fontSize: '50px', color: 'red' }}>
         Error
       </h1>
     </div>
